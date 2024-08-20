e36ac86f4b6f3b94f155560d081d48b9da632077
npetersen <npetersen@fortinet.com>
2024-08-20 15:06:31 -0400

    2

--- app.py
+++ app.py
@@ -4,36 +4,11 @@ import subprocess
 app = Flask(__name__)
 
 def list_interfaces():
-    result = subprocess.run(['ls', '/sys/class/net'], stdout=subprocess.PIPE)
-    interfaces = result.stdout.decode('utf-8').split()
-    interfaces_with_details = []
-    for interface in interfaces:
-        ip_result = subprocess.run(['ip', 'addr', 'show', interface], stdout=subprocess.PIPE)
-        ip_output = ip_result.stdout.decode('utf-8')
-        ip_address = "No IP"
-        for line in ip_output.split('\n'):
-            if 'inet ' in line:
-                ip_address = line.strip().split()[1].split('/')[0]
-                break
-        
-        # Get current degradations
-        tc_result = subprocess.run(['tc', 'qdisc', 'show', 'dev', interface], stdout=subprocess.PIPE)
-        tc_output = tc_result.stdout.decode('utf-8')
-        latency = "None"
-        loss = "None"
-        for line in tc_output.split('\n'):
-            if 'delay' in line:
-                latency = line.strip().split('delay ')[1].split(' ')[0]
-            if 'loss' in line:
-                loss = line.strip().split('loss ')[1].split(' ')[0]
-        
-        interfaces_with_details.append({
-            'name': interface,
-            'ip': ip_address,
-            'latency': latency,
-            'loss': loss
-        })
-    return interfaces_with_details
+    # Dummy data for example purposes
+    return [
+        {'name': 'eth0', 'ip': '192.168.1.1', 'latency': '10ms', 'loss': '0%'},
+        {'name': 'wlan0', 'ip': '192.168.1.2', 'latency': '20ms', 'loss': '1%'}
+    ]
 
 def apply_latency(interface, latency):
     subprocess.run(['sudo', 'tc', 'qdisc', 'add', 'dev', interface, 'root', 'netem', 'delay', latency])
